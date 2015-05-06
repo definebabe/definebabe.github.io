0e728cee11870c2d59d5604feb237cae37ceee70
yurganov <alexey@yurganov.com>
2015-05-06 18:14:36 +0300

    123
    123

---
+++ json.php
@@ -0,0 +1,110 @@
+{
+"query": {
+"count": 1,
+"created": "2015-05-06T14:26:08Z",
+"lang": "ru-RU",
+"results": {
+"channel": {
+"title": "Yahoo! Weather - London, GB",
+"link": "http://www.definebabe.com/models/llno/mia-malkova/",
+"description": "Yahoo! Weather for London, GB",
+"language": "en-us",
+"lastBuildDate": "Wed, 06 May 2015 2:50 pm BST",
+"ttl": "60",
+"location": {
+"city": "London",
+"country": "United Kingdom",
+"region": ""
+},
+"units": {
+"distance": "mi",
+"pressure": "in",
+"speed": "mph",
+"temperature": "F"
+},
+"wind": {
+"chill": "54",
+"direction": "240",
+"speed": "21"
+},
+"atmosphere": {
+"humidity": "82",
+"pressure": "29.65",
+"rising": "1",
+"visibility": "6.21"
+},
+"astronomy": {
+"sunrise": "5:23 am",
+"sunset": "8:29 pm"
+},
+"image": {
+"title": "Yahoo! Weather",
+"width": "47",
+"height": "47",
+"link": "http://weather.yahoo.com",
+"url": "http://l.yurganov.ru/images/2015-05-06_17.29.56.png"
+},
+"item": {
+"title": "Conditions for London, GB at 2:50 pm BST",
+"lat": "51.51",
+"long": "-0.13",
+"link": "http://us.rd.yahoo.com/dailynews/rss/weather/London__GB/*http://weather.yahoo.com/forecast/UKXX0085_f.html",
+"pubDate": "Wed, 06 May 2015 2:50 pm BST",
+"condition": {
+"code": "11",
+"date": "Wed, 06 May 2015 2:50 pm BST",
+"temp": "54",
+"text": "Sasha Gray has 1222222 new gallery published!"
+},
+"description": "\n<img src=\"http://l.yimg.com/a/i/us/we/52/11.gif\"/><br />\n<b>Current Conditions:</b><br />\nLight Rain Shower, 54 F<BR />\n<BR /><b>Forecast:</b><BR />\nWed - Cloudy/Wind. High: 58 Low: 47<br />\nThu - Partly Cloudy. High: 64 Low: 46<br />\nFri - Cloudy. High: 65 Low: 50<br />\nSat - AM Showers/Wind. High: 62 Low: 47<br />\nSun - Partly Cloudy. High: 69 Low: 51<br />\n<br />\n<a href=\"http://us.rd.yahoo.com/dailynews/rss/weather/London__GB/*http://weather.yahoo.com/forecast/UKXX0085_f.html\">Full Forecast at Yahoo! Weather</a><BR/><BR/>\n(provided by <a href=\"http://www.weather.com\" >The Weather Channel</a>)<br/>\n",
+"forecast": [
+{
+"code": "24",
+"date": "6 May 2015",
+"day": "Wed",
+"high": "58",
+"low": "47",
+"text": "Cloudy/Wind"
+},
+{
+"code": "30",
+"date": "7 May 2015",
+"day": "Thu",
+"high": "64",
+"low": "46",
+"text": "Partly Cloudy"
+},
+{
+"code": "26",
+"date": "8 May 2015",
+"day": "Fri",
+"high": "65",
+"low": "50",
+"text": "Cloudy"
+},
+{
+"code": "39",
+"date": "9 May 2015",
+"day": "Sat",
+"high": "62",
+"low": "47",
+"text": "AM Showers/Wind"
+},
+{
+"code": "30",
+"date": "10 May 2015",
+"day": "Sun",
+"high": "69",
+"low": "51",
+"text": "Partly Cloudy"
+}
+],
+"guid": {
+"isPermaLink": "false",
+"content": "UKXX0085_2015_05_10_7_00_BST"
+}
+}
+}
+}
+}
+}
